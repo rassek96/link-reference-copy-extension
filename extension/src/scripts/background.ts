65ce623fd24b57d387e089a9ae50f261f5f42554
Rasmus Karlsson <rassek96@gmail.com>
2020-02-23 16:44:35 +0100

    Added notifications

--- extension/src/scripts/background.ts
+++ extension/src/scripts/background.ts
@@ -1,19 +1,42 @@
 import {Event} from '../lib/entities';
 import {copyToClipboard} from '../helpers';
 
+const displayMessage = (message: string) => {
+  chrome.notifications.create('link-reference-copy-extension', {
+    type: 'basic',
+    message,
+    iconUrl: 'src/assets/link-32px.png',
+    title: '',
+  });
+};
+
 chrome.runtime.onInstalled.addListener(() => {
   chrome.contextMenus.create({
     id: 'menuItem',
     title: 'Copy referenced link to clipboard',
+    contexts: ['all'],
   });
 });
 
 chrome.contextMenus.onClicked.addListener((info, tab) => {
   chrome.tabs.sendMessage(tab.id, Event.GetIdOfClickedElement, result => {
-    if (result && !!result.value && !result.error) {
-      const url = tab.url.split('#')[0];
-      const urlWithReference = `${url}#${result.value}`;
-      copyToClipboard(urlWithReference);
+    if (!result || result.error) {
+      displayMessage(
+        'Something went wrong. Copied URL without anchor to clipboard'
+      );
+      return copyToClipboard(tab.url);
     }
+
+    if (!result.value) {
+      displayMessage(
+        'Cannot anchor to any nearby elements. Copied URL without anchor to clipboard'
+      );
+      return copyToClipboard(tab.url);
+    }
+
+    const url = tab.url.split('#')[0];
+    const urlWithReference = `${url}#${result.value}`;
+    displayMessage('Anchored URL copied to clipboard');
+    copyToClipboard(urlWithReference);
   });
 });
--- extension/src/scripts/content-script.ts
+++ extension/src/scripts/content-script.ts
@@ -12,15 +12,21 @@ document.addEventListener(
   true
 );
 
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+chrome.runtime.onMessage.addListener((request: Event, _, sendResponse) => {
   if (request === Event.GetIdOfClickedElement) {
     if (!clickedElement) {
       return sendResponse({value: null, error: true});
     }
 
     try {
-      const id = traverseToFindId(clickedElement);
-      return sendResponse({value: id, error: !id});
+      let id = traverseToFindId(clickedElement);
+
+      // If no id was found, check parent element
+      if (!id && !!clickedElement.parentNode) {
+        id = traverseToFindId(clickedElement.parentNode);
+      }
+
+      return sendResponse({value: !!id ? id : null, error: false});
     } catch (e) {
       return sendResponse({value: null, error: true});
     }
